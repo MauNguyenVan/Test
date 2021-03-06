ba8b850b7cd176a363a3b108b686666868680bc2
Mau Nguyen Van <nguyenvanmau9296@gmail.com>
2021-03-06 11:37:26 +0700

    Add project files.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,40 @@
+﻿#region NameSpace
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Numerics;
+using System.Text;
+using System.IO;
+using System.Media;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics.CodeAnalysis;
+#endregion
+#nullable enable
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // path to the csv file
+            string path = "test.csv";
+
+            string[] lines = System.IO.File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                string[] columns = line.Split(',');
+                foreach (string column in columns)
+                {
+                    System.Console.Write(column+"\t");
+                }
+                System.Console.WriteLine(Environment.TickCount);
+            }
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public int XXX { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            Binding binding = new Binding("Text", textBox1, "Text", true, DataSourceUpdateMode.OnPropertyChanged);
+
+     
+            this.DataBindings.Add("Text",textBox1,"Name");
+
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //this.Text = XXX.ToString();
+        }
+    }
+}
