c561cb911f467fbaf6921b71828565f7c7e1bafb
Mau Nguyen Van <nguyenvanmau9296@gmail.com>
2021-04-15 00:21:33 +0700

    trtrtr

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text.Json;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
+using System.Diagnostics;
 
 namespace ConsoleTest
 {
@@ -22,12 +23,12 @@ namespace ConsoleTest
             if(vs>1)
             {
                  WebClient client = new WebClient{};
-                 client.DownloadFile("https://drive.google.com/uc?export=download&id=19WKnk8OvD9peNbEO_wdREnEcAuM6klTp","SetupRevitExpressTools.msi");
-                client.DownloadFile("https://drive.google.com/uc?export=download&id=1181v4UEzouQKvsgRR6rxfmvZuzX98srM","1.exe");
+                 //client.DownloadFile("https://drive.google.com/uc?export=download&id=19WKnk8OvD9peNbEO_wdREnEcAuM6klTp","SetupRevitExpressTools.msi");
+               // client.DownloadFile("https://drive.google.com/uc?export=download&id=1181v4UEzouQKvsgRR6rxfmvZuzX98srM","1.exe");
                
             }
-
-
+string s = "Revit 2020";
+     KillApplication(s);
             Console.WriteLine(content);
         }
         public static string GetLatestVersion()
@@ -50,11 +51,39 @@ namespace ConsoleTest
 
             return latestVersion;
         }
+        
+       
+        public static void KillApplication(string names)
+        {
+            System.Diagnostics.Process[] procs = null;
+
+            try
+            {
+                procs = Process.GetProcessesByName(names);
+foreach (var item in procs)
+{
+    System.Console.WriteLine(item.MachineName);
+}
+                // Process mspaintProc = procs[0];
+
+                // if (!mspaintProc.HasExited)
+                // {
+                //     mspaintProc.Kill();
+                // }
+            }
+            finally
+            {
+                if (procs != null)
+                {
+                    foreach (Process p in procs)
+                    {
+                        p.Dispose();
+                    }
+                }
+            }
+        }
 
     }
-    static class dsds
-    {
-        string Data(this MyClass instance)
-    }
+    
 
 }
