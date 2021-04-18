c48f09d7e5b159b364ca0018a610ab8179547936
Mau Nguyen Van <nguyenvanmau9296@gmail.com>
2021-04-19 00:31:02 +0700

    gfgf

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -7,83 +7,23 @@ using System.Text.Json;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Diagnostics;
-
+//#nullable disable
 namespace ConsoleTest
 {
-
+    
     class Program
     {
-        private const string uriTextVersion = "https://drive.google.com/drive/folders/10KUAF7nrogEtV8v2jLCDO9uJ8UlXbJG0";
 
         public static void Main()
         {
-           nint yt =4;
-            string content = GetLatestVersion();
-            var vs=  Convert.ToDouble(content);
-            if(vs>1)
-            {
-                 WebClient client = new WebClient{};
-                 //client.DownloadFile("https://drive.google.com/uc?export=download&id=19WKnk8OvD9peNbEO_wdREnEcAuM6klTp","SetupRevitExpressTools.msi");
-               // client.DownloadFile("https://drive.google.com/uc?export=download&id=1181v4UEzouQKvsgRR6rxfmvZuzX98srM","1.exe");
-               
-            }
-string s = "Revit 2020";
-     KillApplication(s);
-            Console.WriteLine(content);
+            string? x=null;
+            var fdf = x;
+            object? fdfd= null;
+           System.Console.WriteLine(fdfd!.ToString());
         }
-        public static string GetLatestVersion()
-        {
-            string latestVersion = string.Empty;
 
-            try
-            {
-
-                    WebClient client = new WebClient
-                    {
-                        Encoding = Encoding.UTF8
-                    };
-                    string reply = client.DownloadString("https://drive.google.com/uc?export=download&id=1i-BeOoS7bljmU1DGLUh7RJJZxjJcW9Da");
-                return reply;
-            }
-            catch (Exception)
-            {
-            }
-
-            return latestVersion;
-        }
-        
-       
-        public static void KillApplication(string names)
-        {
-            System.Diagnostics.Process[] procs = null;
-
-            try
-            {
-                procs = Process.GetProcessesByName(names);
-foreach (var item in procs)
-{
-    System.Console.WriteLine(item.MachineName);
-}
-                // Process mspaintProc = procs[0];
-
-                // if (!mspaintProc.HasExited)
-                // {
-                //     mspaintProc.Kill();
-                // }
-            }
-            finally
-            {
-                if (procs != null)
-                {
-                    foreach (Process p in procs)
-                    {
-                        p.Dispose();
-                    }
-                }
-            }
-        }
 
     }
-    
+
 
 }
