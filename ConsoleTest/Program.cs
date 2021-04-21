5bb4ef1d8c02212880ae1ad5a89ca766978dbe89
Mau Nguyen Van <nguyenvanmau9296@gmail.com>
2021-04-22 00:08:15 +0700

    jiu

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -13,13 +13,37 @@ namespace ConsoleTest
     
     class Program
     {
-
+enum ee 
+{a=321,b=3,c=4}
         public static void Main()
         {
-            string? x=null;
-            var fdf = x;
-            object? fdfd= null;
-           System.Console.WriteLine(fdfd!.ToString());
+            
+     var x=ee.GetNames();
+            List<int> ii=new() {10,20,30};
+            var e = ii.GetEnumerator();
+           System.Console.WriteLine(e.GetType().Name);
+            foreach (var i in ii)
+            {
+               
+               // ii.Remove(i);
+                // ii.Remove(20);
+                 // ii.Remove(30);
+                 
+                // i=6;
+               // ii.Remove(10);
+               System.Console.WriteLine(ii.Count);
+                 System.Console.WriteLine(i);
+
+            }
+             System.Console.WriteLine(ii.Count);
+             foreach (var i in ii)
+            {
+               
+            
+                 System.Console.WriteLine(i);
+
+            }
+           
         }
 
 
